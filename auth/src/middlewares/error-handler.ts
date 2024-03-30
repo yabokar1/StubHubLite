afbbe6a853a5b311be1d1ebedecb2089c47d9c2c
yabokar1 <yabokar@gmail.com>
2024-03-30 06:46:15 -0400

    Added CustomError class

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -10,14 +9,8 @@ export const errorHandler = (
 ) => {
   console.log("Something went wrong", err);
 
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    return res
-      .status(err.statusCode)
-      .send({ errors: [{ message: err.serializeErrors() }] });
   }
 
   res.status(400).send({
